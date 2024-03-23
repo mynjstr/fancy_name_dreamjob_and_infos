97a91fdffb1b059f0cf08a829f0ccfb6ab178f15
mynjstr <jestertiedra@gmail.com>
2024-03-23 21:22:55 +0800

    Added Pseudocode

---
+++ fancy_name_dreamjob_and_personalinfos.py
@@ -0,0 +1,8 @@
+#pseudocode
+
+# import pyfiglet
+# import termcolor
+# ask user for inputs
+# use pyfiglet to make a sentence_art with the inputs
+# print the sentence_art with the use of termcolor
+
