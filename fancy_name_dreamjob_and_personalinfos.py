2e51761cc8472d278209a92e905e2de0326a1bb4
mynjstr <jestertiedra@gmail.com>
2024-03-23 21:28:37 +0800

    Import termcolor

--- fancy_name_dreamjob_and_personalinfos.py
+++ fancy_name_dreamjob_and_personalinfos.py
@@ -3,6 +3,7 @@
 # import pyfiglet
 import pyfiglet
 # import termcolor
+from termcolor import colored
 # ask user for inputs
 # use pyfiglet to make a sentence_art with the inputs
 # print the sentence_art with the use of termcolor
