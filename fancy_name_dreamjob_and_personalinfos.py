915d7dae0306ffb67258d914cabdd2b61d627ed8
mynjstr <jestertiedra@gmail.com>
2024-03-23 21:47:12 +0800

    Ask user for inputs

--- fancy_name_dreamjob_and_personalinfos.py
+++ fancy_name_dreamjob_and_personalinfos.py
@@ -5,6 +5,12 @@ import pyfiglet
 # import termcolor
 from termcolor import colored
 # ask user for inputs
+name = input("Please enter your name: ")
+dream_job = input("Please enter your dream job: ")
+age = int(input("Please enter your age: "))
+hometown = input("Please enter your hometown: ")
+birthdate = input("Please enter your birthdate: ")
+pet_peeves = input("Please enter your Pet peeve: ")
 # use pyfiglet to make a sentence_art with the inputs
 # print the sentence_art with the use of termcolor
 
