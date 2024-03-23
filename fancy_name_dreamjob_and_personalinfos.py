f4b089f42d201f2bb3f735b7dcf8e428cd3f962f
mynjstr <jestertiedra@gmail.com>
2024-03-23 22:22:48 +0800

    Use pyfiglet to make a sentence_art with the inputs

--- fancy_name_dreamjob_and_personalinfos.py
+++ fancy_name_dreamjob_and_personalinfos.py
@@ -12,5 +12,6 @@ hometown = input("Please enter your hometown: ")
 birthdate = input("Please enter your birthdate: ")
 pet_peeves = input("Please enter your Pet peeve: ")
 # use pyfiglet to make a sentence_art with the inputs
+sentence_art = pyfiglet.figlet_format(f"Hi, my name is {name}, I'm from {hometown}, I was born on {birthdate}, I'm {age} years old, my pet peeve is {pet_peeves} and being a {dream_job} is my dream job", font="roman",width =160)
 # print the sentence_art with the use of termcolor
 
